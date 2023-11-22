3a7e72250cc3bcf50fa5f085d428ad2b65dbff42
ayoub <ayoub.barich@etu.univ-amu.fr>
2023-11-22 23:17:38 +0100

    Changes to be committed: 	modified:   ExampleProg.txt 	modified:   SyntaxAnalayser.py 	modified:   __pycache__/ErrorHandler.cpython-310.pyc

--- SyntaxAnalayser.py
+++ SyntaxAnalayser.py
@@ -2,6 +2,8 @@
 from Reader import Reader
 from ErrorHandler import RaiseError
 from Tokens import TYPE
+import string
+
 # CARLU=''
 NUM_LIGNE=0
 INDEX = 0
@@ -12,10 +14,12 @@ MAXLENSTR=50
 TestPathComments = "./CommentsTest.txt"
 TestPathNumberTest = "./NumberTest.txt"
 TestPathProgram = "./ExampleProg.txt"
+ALPHABET = list(string.ascii_letters)
 class SyntaxAnalayser():
 
     def __init__(self,program_file_path):
         self.prog = Reader(program_file_path).content
+        print(ALPHABET)
         while True:
             self.LIRE_CAR()
 
@@ -27,13 +31,14 @@ class SyntaxAnalayser():
         global NUM_LIGNE
         global INDEX
         
-        if not self.end():
+       
+        if  not self.end():
+
             match self.prog[INDEX]:
                 
                 case '\n':
                     NUM_LIGNE+=1
-                    INDEX += 1
-                    
+                    INDEX += 1 
                 case ' '|'    ':
                     self.SAUTER_SEPARATEUR()
                     
@@ -41,18 +46,21 @@ class SyntaxAnalayser():
                     self.SAUTER_COMMENT()
                 case '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9':
                     self.RECO_ENTIER()
-                    print("Nombre",NOMBRE)
+                    #print("Nombre",NOMBRE)
                 case '\'':
                     self.RECO_CHAINE()
-                case _:
-                    print(self.prog[INDEX])
-                    INDEX += 1
+
+            if self.prog[INDEX] in ALPHABET:
+                self.RECO_IDENT_OU_MOT_RESERVE()
+            else :
+                INDEX +=1
+
             
 
-            if not self.end():
-                print(self.prog[INDEX])
-            else:
-                RaiseError.END_OF_FILE()
+            # if not self.end():
+            #     print(self.prog[INDEX])
+            # else:
+            #     RaiseError.END_OF_FILE()
         else:
             RaiseError.END_OF_FILE()
 
@@ -74,16 +82,15 @@ class SyntaxAnalayser():
         while counter != 0 :  
 
             if not self.end():
-                print('dans commentaire',self.prog[INDEX])
-                print(counter)
+                #print('dans commentaire',self.prog[INDEX])
+                #print(counter)
                 if self.prog[INDEX] == '{':
                     counter += 1      
                 elif self.prog[INDEX] == '}':
                     counter -= 2
                 INDEX += 1
-            else:
-                break
-        if (counter !=0):
+            
+        if(counter != 0 ):
             RaiseError.OPEN_COMMENT_FIELD()
 
 
@@ -91,6 +98,7 @@ class SyntaxAnalayser():
         return (self.prog[INDEX] == '0') |( self.prog[INDEX] == '1') | (self.prog[INDEX] == '2')| (self.prog[INDEX] == '3') | (self.prog[INDEX] == '4') | (self.prog[INDEX] == '5') | (self.prog[INDEX] == '6') | (self.prog[INDEX] == '7') | (self.prog[INDEX] == '8')| (self.prog[INDEX] == '9') 
 
     def end(self):
+        global INDEX
         return INDEX >= len(self.prog)  
 
     def RECO_ENTIER(self) :
@@ -107,34 +115,80 @@ class SyntaxAnalayser():
         if NOMBRE > MAXINT:
             RaiseError.MAXINT()
         return (TYPE.ent ,NOMBRE)
+    
 
     def RECO_CHAINE(self):
-        CHAINE =''
         global INDEX
-    
+        INDEX+=1
+        #CHAINE = self.prog[INDEX]
+        CHAINE =""
+
         while not self.end():
-            
-            CHAINE+=self.prog[INDEX]
-            INDEX+=1
-            while not self.end() and self.prog[INDEX] != '\'':
-                CHAINE+=self.prog[INDEX]
+            #print("CHAINE",CHAINE)
+            #print("Prog",self.prog[INDEX])
+            while self.prog[INDEX] =="\'":
                 INDEX+=1
-            if not self.end():
-                if self.prog[INDEX] == '\'':
-                    if not self.end() and self.prog[INDEX+1] != '\'':
-                        INDEX+=1
-                        CHAINE+=self.prog[INDEX]
-                        break
-                    elif not self.end() :
-                        CHAINE += '\"'
-                        INDEX+=1
+            
+                if self.prog[INDEX] !="\'":
+                    #print("RETT")
+                    return TYPE.ch
+                INDEX+=1
+                break
+                
+            while self.prog[INDEX] !="\'":
+                CHAINE += self.prog[INDEX]
+                INDEX +=1
+                
 
 
-        if len(CHAINE) > MAXLENSTR:
-            print('CHAINE = ',CHAINE)
-            RaiseError.MAX_LEN_STR_REACHED()
-        print('CHAINE = ',CHAINE)
-        return (TYPE.ch , CHAINE)
+    def RECO_IDENT_OU_MOT_RESERVE(self):
+        global INDEX
+        global CHAINE
+        while self.condition():
+            CHAINE += self.prog[INDEX]
+            INDEX +=1 
+    
+        return
+
+        
+    def condition(self):
+        global INDEX
+        global ALPHABET
+        return self.prog[INDEX] in ALPHABET or self.is_entier() or self.prog[INDEX] == "_"
+
+        
+        
+
+
+
+
+    # def RECO_CHAINE(self):
+    #     CHAINE =''
+    #     global INDEX
+    
+    #     while not self.end():
+    #         CHAINE+=self.prog[INDEX]
+    #         INDEX+=1
+    #         while not self.end() and self.prog[INDEX] != '\'':
+    #             CHAINE+=self.prog[INDEX]
+    #             INDEX+=1
+    #         if not self.end():
+    #             if self.prog[INDEX] == '\'':
+    #                 if not self.end() and self.prog[INDEX+1] != '\'':
+    #                     INDEX+=1
+    #                     CHAINE+=self.prog[INDEX]
+    #                     break
+    #                 elif not self.end() :
+    #                     CHAINE += '\"'
+    #                     INDEX+=1
+
+
+    #     if len(CHAINE) > MAXLENSTR:
+    #         print('CHAINE = ',CHAINE)
+    #         RaiseError.MAX_LEN_STR_REACHED()
+    #     print('CHAINE = ',CHAINE)
+    #     return (TYPE.ch , CHAINE)         break
+        
 
 
 
@@ -156,5 +210,5 @@ class SyntaxAnalayser():
 ######################Test#########################
 
 
-sy =SyntaxAnalayser(TestPathProgram)
+sy = SyntaxAnalayser(TestPathProgram)
 
